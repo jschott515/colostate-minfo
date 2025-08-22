bb7c9d5fe94b0e138efe94924512c812d290c559
Jordan Schott <jschott515@gmail.com>
2025-08-22 10:56:25 -0500

    Stricter checking for header contents, improve docs

--- minfo/__init__.py
+++ minfo/__init__.py
@@ -1,3 +1,4 @@
+import enum
 import typing
 
 import requests
@@ -7,7 +8,24 @@ import bs4
 MACHINE_STATS_URL = "https://www.cs.colostate.edu/machinestats/"
 
 
-def fetch_machine_info() -> typing.Sequence[typing.MutableMapping[str, str]]:
+class MinfoFields(enum.StrEnum):
+    """Enumeration representing the header fields in the machinestats table.
+    Order and string value of enumeration must match the contents of the table.
+    """
+    HOST = 'Hostname'
+    LOCATION = 'Location'
+    DATE = 'Date'
+    TIME = 'Time'
+    CPU_USAGE = 'CPU Usage (%)'
+    LOAD_AVG = 'Load Average'
+    MEM_USAGE = 'Memory Usage (%)'
+    GPU_USAGE = 'GPU Memory Usage (%)'
+    RDP_SESSIONS = 'RDP Sessions Used'
+    UP_TIME = 'Up Time'
+    USERS = 'Logged-in Users'
+
+
+def fetch_machine_info() -> typing.Sequence[typing.MutableMapping[MinfoFields, str]]:
     response = requests.get(MACHINE_STATS_URL)
     response.raise_for_status()
     soup = bs4.BeautifulSoup(response.text, 'html.parser')
@@ -21,11 +39,13 @@ def fetch_machine_info() -> typing.Sequence[typing.MutableMapping[str, str]]:
 
     headers = [th.get_text(strip=True) for th in header.find_all('th')]
     assert headers is not None, "Header Row contained no entries..."
+    assert len(headers) == len(MinfoFields), "Header Row does not contain the expected number of fields..."
+    assert all(field == minfo_field.value for field, minfo_field in zip(headers, MinfoFields)), "Header Row contents do not match the expected fields..."
 
-    machines: typing.Sequence[typing.MutableMapping[str, str]] = []
+    machines: typing.Sequence[typing.MutableMapping[MinfoFields, str]] = []
     for row in rows:
         cols = [td.get_text(strip=True) for td in row.find_all('td')]
-        assert len(cols) == len(headers), f"Table row contained {len(cols)} columns, expected {len(headers)}"
-        info_dict = dict(zip(headers, cols))
+        assert len(cols) == len(MinfoFields), f"Table row contained {len(cols)} columns, expected {len(MinfoFields)}"
+        info_dict = dict(zip(MinfoFields, cols))
         machines.append(info_dict)
     return machines
--- minfo/tools/launch_ssh.py
+++ minfo/tools/launch_ssh.py
@@ -1,3 +1,5 @@
+"""Launches an SSH Session using the machine with the current lowest utilization.
+"""
 import argparse
 import typing
 import subprocess
@@ -11,7 +13,7 @@ TARGET = '{username}@{machine}.cs.colostate.edu'
 
 
 def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("USERNAME", type=str,
                         help="Username used for SSH connection")
     parser.add_argument("--ssh", type=str, default=SSH_PATH,
@@ -20,14 +22,17 @@ def parse_args() -> argparse.Namespace:
     return cfg
 
 
-def get_min_usage_host(info: typing.Sequence[typing.MutableMapping[str, str]]) -> str:
-    device_info = min(info, key=lambda x: x['CPU Usage (%)'])
-    return device_info['Hostname']
+def get_min_usage_host(info: typing.Sequence[typing.MutableMapping[minfo.MinfoFields, str]]) -> str:
+    device_info = min(info, key=lambda x: x[minfo.MinfoFields.CPU_USAGE])
+    return device_info[minfo.MinfoFields.HOST]
 
 
 if __name__ == "__main__":
     cfg = parse_args()
+    username = typing.cast(str, cfg.USERNAME)
+    ssh_path = typing.cast(str, cfg.ssh)
+
     info = minfo.fetch_machine_info()
     machine = get_min_usage_host(info)
-    target = TARGET.format(username=cfg.USERNAME, machine=machine)
-    subprocess.run([cfg.ssh, target])
+    target = TARGET.format(username=username, machine=machine)
+    subprocess.run([ssh_path, target])
